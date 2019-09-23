fe4816dd09a983b6989ab96211338abeb954e9c8
ken-marui001 <noobgladiator001@gmail.com>
2019-09-23 11:59:10 +0900

    Usersテーブルから特定のカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_043628) do
+ActiveRecord::Schema.define(version: 2019_09_23_015843) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name_kanji", null: false
-    t.string "nmae_kana", null: false
-    t.datetime "birth_data", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
