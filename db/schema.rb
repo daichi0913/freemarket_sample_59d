1fb569cdbaa61e67fb5a943d722782ea6fe9809c
ken-marui001 <noobgladiator001@gmail.com>
2019-09-23 12:33:04 +0900

    userのルーティングとコントローラの更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_23_030032) do
+ActiveRecord::Schema.define(version: 2019_09_23_031512) do
+
+  create_table "user_details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name_kanji", null: false
+    t.string "name_kana", null: false
+    t.text "address", null: false
+    t.integer "phone_number", null: false
+    t.datetime "birth_date", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_details_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
