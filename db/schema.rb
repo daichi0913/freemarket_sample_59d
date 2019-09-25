bece3730814545e85a81dad98806cd2af4e7b9b4
ken-marui001 <noobgladiator001@gmail.com>
2019-09-25 10:08:24 +0900

    rspecインストール、categoryテーブルのseed作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_23_031512) do
+ActiveRecord::Schema.define(version: 2019_09_24_101124) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "parent_id"
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "user_details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
