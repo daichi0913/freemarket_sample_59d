a72702e71d0eeccf74ea6dfc8a2a4bad64366105
yoshihide88 <mini8686mini0112@gmail.com>
2019-10-02 17:53:23 +0900

    出品ページの画像

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,9 @@ class ItemsController < ApplicationController
     @categories = Category.where(parent_id: nil)
     region_id = params[:id].to_i
     @regions = Region.all
-    # @item_images = Item_image.create
+    @item_image = ItemImage.new
+    @item = Item.new
+    5.times { @item.item_images.build }
   end
   
   def show
@@ -12,6 +14,11 @@ class ItemsController < ApplicationController
   end
 
   def create
-
+    binding.pry
   end
+
+  # private
+  #   def create_params
+  #   params.require(:item).permit(item_image_attributes: [:image])
+  #   end
 end
