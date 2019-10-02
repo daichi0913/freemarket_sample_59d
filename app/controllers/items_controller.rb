0973e88fcfff2570410d96911abeac973d1da31e
Yuk1111 <gin_yuk1010@yahoo.co.jp>
2019-10-02 18:11:02 +0900

    faker
    DB追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,13 @@ class ItemsController < ApplicationController
     @categories = Category.where(parent_id: nil)
     region_id = params[:id].to_i
     @regions = Region.all
+    @item_ladies = Item.find(category_id@item.category.parent.parent.name)per(10).order("created_at DESC")
+    @item_mens = Item.per(10).order("created_at DESC")
+    @item_appliances = Item.per(10).order("created_at DESC")
+    @item_toys = Item.per(10).order("created_at DESC")
+    @item_mens = Item.per(10).order("created_at DESC")
+
+
     # @item_images = Item_image.create
 
   end
