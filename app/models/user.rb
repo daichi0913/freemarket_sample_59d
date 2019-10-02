b110203cba9aa4a35b21088ea009e4657cdd93bf
ken-marui001 <noobgladiator001@gmail.com>
2019-10-02 13:19:05 +0900

    新規登録・ログインのバリデーションとテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_one_attached :image
+  has_one :user_detail
+  accepts_nested_attributes_for :user_detail
+  validates :nickname, presence: true, length: {maximum: 20}
+  validates :password, presence: true, length: {minimum: 7, maximum: 128},  format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,128}+\z/i}
 
 end
---
+++ spec/factories/user_details.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user_detail do
+    family_name_kana  {Gimei.name.last.katakana}
+    first_name_kana {Gimei.name.first.katakana}
+    # name_kana {family_name_kana+" "+first_name_kana}
+    family_name_kanji {Gimei.name.last.kanji}
+    first_name_kanji {Gimei.name.first.kanji}
+    # name_kanji {family_name_kanji+" "+first_name_kanji}
+    birth_date {Faker::Date.birthday(min_age: 18, max_age: 65).strftime("%Y/%m/%d %H:%M")}
+    user
+  end
+end
