b226c41eca98753b10e153d9d60f763d8a3ecec2
Yuk1111 <gin_yuk1010@yahoo.co.jp>
2019-10-03 12:53:14 +0900

    トップページ
    ブランド追加

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -10,7 +10,11 @@ class ToppagesController < ApplicationController
     @item_mens = []
     @item_appliances = []
     @item_toys = []
+    @item_brand_chanel= []
+    @item_brand_lv= []
+    @item_brand_supreme = []
     @item_brand_nike = []
+
     # @item_mens = Item.per(10).order("created_at DESC")
     @items.each do |item|
       if item.category.parent.parent.name == "レディース"
@@ -33,10 +37,26 @@ class ToppagesController < ApplicationController
           @item_toys << item
         end
 
-      if item.brand.name == "ナイキ"
+      if item.brand.name == "シャネル"
+        if(@item_brand_chanel.length < 10)
+          @item_brand_chanel << item
+        end
+
+      elsif item.brand.name == "ルイヴィトン"
+        if(@item_brand_lv.length < 10)
+          @item_brand_lv << item
+        end
+
+      elsif item.brand.name == "シュプリーム"
+        if(@item_brand_supreme.length < 10)
+          @item_brand_supreme << item
+        end
+
+      elsif item.brand.name == "ナイキ"
         if(@item_brand_nike.length < 10)
           @item_brand_nike << item
         end
+
       end
     end
   end
