f80af79fd196bc0b5bd64179252a4375ddd47801
ken-marui001 <noobgladiator001@gmail.com>
2019-10-04 16:42:36 +0900

    微修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,18 +49,18 @@ end
 Item.create!(
   {user_id: '1', category_id: '18', brand_id: '1', region_id: '1', name: 'セーター', size: 'Mサイズ', item_status: '新品', shipping_fee: '購入者負担', shipping_way: '普通郵便', days: '3日以内に発送', price: '1000', explain: 'とっても可愛いです。', created_at: '20190930', updated_at: '20191001'}
   )
-
+ItemImage.create!({})
 # ↓レディース新着表示用
 30.times do
      Item.create!(
-      {user_id: '1', category_id: rand(18..31), brand_id: rand(1..4), region_id: rand(1..47), name: Faker::Dessert.variety, size: Faker::Games::Pokemon.name, item_status: Faker::Creature::Animal.name, shipping_fee: Faker::Superhero.name, shipping_way: Faker::Movies::HarryPotter.location, days: Faker::TvShows::BojackHorseman.character, price: Faker::Number.between(from: 1, to: 10000000), explain: Faker::TvShows::BojackHorseman.tongue_twister, created_at: Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all), updated_at: Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all)}
+      {user_id: rand(1..100), category_id: rand(18..31), brand_id: rand(1..4), region_id: rand(1..47), name: Faker::Dessert.variety, size: Faker::Games::Pokemon.name, item_status: Faker::Creature::Animal.name, shipping_fee: Faker::Superhero.name, shipping_way: Faker::Movies::HarryPotter.location, days: Faker::TvShows::BojackHorseman.character, price: Faker::Number.between(from: 1, to: 10000000), explain: Faker::TvShows::BojackHorseman.tongue_twister, created_at: Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all), updated_at: Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all)}
       )
   end
 
   # ↓メンズ新着表示用
   30.times do
     Item.create!(
-     {user_id: '1', category_id: rand(140..151), brand_id: rand(1..4), region_id: rand(1..47), name: Faker::Dessert.variety, size: Faker::Games::Pokemon.name, item_status: Faker::Creature::Animal.name, deal_status: Faker::TvShows::StrangerThings.character, shipping_fee: Faker::Superhero.name, shipping_way: Faker::Movies::HarryPotter.location, days: Faker::TvShows::BojackHorseman.character, price: Faker::Number.between(from: 1000, to: 9000), explain: Faker::TvShows::BojackHorseman.tongue_twister, created_at: Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all), updated_at: Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all)}
+     {user_id: rand(1..100), category_id: rand(140..151), brand_id: rand(1..4), region_id: rand(1..47), name: Faker::Dessert.variety, size: Faker::Games::Pokemon.name, item_status: Faker::Creature::Animal.name, deal_status: Faker::TvShows::StrangerThings.character, shipping_fee: Faker::Superhero.name, shipping_way: Faker::Movies::HarryPotter.location, days: Faker::TvShows::BojackHorseman.character, price: Faker::Number.between(from: 1000, to: 9000), explain: Faker::TvShows::BojackHorseman.tongue_twister, created_at: Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all), updated_at: Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all)}
      )
  end
 
