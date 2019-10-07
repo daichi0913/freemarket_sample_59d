890b221e4511e4e14892079120ca333e5836030b
daichi0913 <tonan0913cr@gmail.com>
2019-10-07 12:03:57 +0900

    デプロイ変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
+# set :default_env, {
+#   rbenv_root: "/usr/local/rbenv",
+#   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+#   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+#   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+# }
 
 set :linked_files, fetch(:linked_files, []).push("config/master.key")
 # config valid for current version and patch releases of Capistrano
