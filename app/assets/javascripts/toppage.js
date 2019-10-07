f2f0dd3a4493a26e11496a956cdccb3e16dc38f1
ken-marui001 <noobgladiator001@gmail.com>
2019-10-08 00:26:12 +0900

    make auto slide

--- app/assets/javascripts/toppage.js
+++ app/assets/javascripts/toppage.js
@@ -1,16 +1,39 @@
 $(()=>{
-  function slideview() {
+  function slide_view(index, inv=false){
+    //this function's default is that image move to left
+
+    //get view width because image's width is variable
+    const width = $($(".slider-image")[0]).width();
+    let num = index+1;
+    
+    //inv means inverse slide. image move to right
+    if(inv){
+      num = indx-1;
+      if(num< 0){
+        num = $(".slider-image").length -1;
+      }
+    }else{
+      if(num===$(".slider-image").length){
+        num = 0;
+      }
+    }
+
+    //move active-class on .slider-image and .dots
+    $($(".slider-image")[index]).removeClass('active');
+    $($(".dots")[index]).removeClass('active');
+    $($(".slider-image")[num]).addClass('active');
+    $($(".dots")[num]).addClass('active');
+    //slide
+    $(".slides").animate({scrollLeft: width*num}, 300);
+  }
+  
+  function auto_slide() {
     if(!(location.pathname.match(/^\/$/))){
       return;
     }
-    console.log("slide");
-    // if($($(".slider-image")[0]).css('z-index')==="0"){
-    //   $($(".slider-image")[0]).css('z-index', "-1");
-    //   $($(".slider-image")[1]).css('z-index', "0");
-    // }else{
-    //   $($(".slider-image")[0]).css('z-index', "0");
-    //   $($(".slider-image")[1]).css('z-index', "-1");
-    // }
+    let index = $('.slider-image').index($('.slider-image.active')[0]);
+    slide_view(index);
+    console.log(index);
   }
   $(document).on('turbolinks:load', function(){
     
@@ -20,6 +43,6 @@ $(()=>{
       $(".item-photo-main").animate({scrollLeft: index * 300}, 300);
     });
 
-    setInterval(slideview, 5000);
+    setInterval(auto_slide, 5000);
   })
 })
