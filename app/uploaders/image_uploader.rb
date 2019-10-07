4be5ddf2fa469bfce8075c96cf63bc15b2940afd
daichi0913 <tonan0913cr@gmail.com>
2019-10-07 16:58:05 +0900

    S3完成

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,6 +4,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+
+
+
   if Rails.env.development? || Rails.env.test?
     storage :file
   else
@@ -15,6 +18,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+
+  
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
