d9f3e7dfb575bc8b25ae5ca68cfadbd98a47e239
ken-marui001 <noobgladiator001@gmail.com>
2019-10-08 00:35:34 +0900

    Update toppage.js

--- app/assets/javascripts/toppage.js
+++ app/assets/javascripts/toppage.js
@@ -1,14 +1,16 @@
 $(()=>{
-  function slide_view(index, inv=false){
+  function slide_view(inv=false){
     //this function's default is that image move to left
 
+    //get index to know current .slider-image's place
     //get view width because image's width is variable
+    const index = $('.slider-image').index($('.slider-image.active')[0]);
     const width = $($(".slider-image")[0]).width();
     let num = index+1;
     
     //inv means inverse slide. image move to right
     if(inv){
-      num = indx-1;
+      num = index-1;
       if(num< 0){
         num = $(".slider-image").length -1;
       }
@@ -31,9 +33,8 @@ $(()=>{
     if(!(location.pathname.match(/^\/$/))){
       return;
     }
-    let index = $('.slider-image').index($('.slider-image.active')[0]);
-    slide_view(index);
-    console.log(index);
+    slide_view();
+    // console.log(index);
   }
   $(document).on('turbolinks:load', function(){
     
@@ -42,7 +43,12 @@ $(()=>{
       // $(".item-photo-main")[0].scrollLeft = index * 300;
       $(".item-photo-main").animate({scrollLeft: index * 300}, 300);
     });
-
+    $('.slider-controller-left').on("click", function(){
+      slide_view(true);
+    })
+    $('.slider-controller-right').on("click", function(){
+      slide_view();
+    })
     setInterval(auto_slide, 5000);
   })
 })
