bba6f89efb0fafdd921535c07d31512bffc5d19d
ken-marui001 <noobgladiator001@gmail.com>
2019-10-08 15:15:02 +0900

    Update toppage.js

--- app/assets/javascripts/toppage.js
+++ app/assets/javascripts/toppage.js
@@ -37,12 +37,6 @@ $(()=>{
     // console.log(index);
   }
   $(document).on('turbolinks:load', function(){
-    
-    $('.photo-slide img').on("mouseenter", function(){
-      var index = $('.photo-slide img').index(this);
-      // $(".item-photo-main")[0].scrollLeft = index * 300;
-      $(".item-photo-main").animate({scrollLeft: index * 300}, 300);
-    });
     $('.slider-controller-left').on("click", function(){
       slide_view(true);
     })
