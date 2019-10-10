c580fbdf71282358e0c7e1ceac9473ccce01ca06
ken-marui001 <noobgladiator001@gmail.com>
2019-10-10 20:05:23 +0900

    ターボリンクスによるエラーの回避

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,9 +1,13 @@
 $(function(){
-  $('#item-delete-modal-on').on('click', function(){
-    $("#item-delete-modal").addClass('show');
-  })
-
-  $('.modal').on('click', function(){
-    $(this).removeClass('show');
-  })
+  $(document).on('turbolinks:load', function(){
+    $('#item-delete-modal-on').on('click', function(){
+      $("#item-delete-modal").addClass('show');
+      $('.modal-banner').animate({"margin": "20px auto", "opacity": "1"}, 400);
+    })
+  
+    $('.modal').on('click', function(){
+      $(this).removeClass('show');
+      $('.modal-banner').css({"margin": "8px auto", "opacity": "0"});
+    })
+  })  
 })
