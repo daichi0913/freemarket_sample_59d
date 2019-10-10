deffd506c2f5694398b13787d072830e90edca2f
daichi0913 <tonan0913cr@gmail.com>
2019-10-10 14:16:09 +0900

    クレジット完成

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -4,7 +4,7 @@ class DealsController < ApplicationController
   end
 
   def purchase
-    Payjp.api_key = PAYJP_SECRET_KEY
+    Payjp.api_key = Rails.application.credentials.PAYJP_PRIVATE_KEY
     Payjp::Charge.create(currency: 'jpy', amount: 1000, card: params['payjp-token'])
     redirect_to root_path, notice: "支払いが完了しました"
   end
