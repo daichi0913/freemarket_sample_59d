9c1878dcaef25adceba7d8ce4644b46f87315acb
daichi0913 <tonan0913cr@gmail.com>
2019-10-10 18:54:03 +0900

    本番環境変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,14 +32,12 @@ class ItemsController < ApplicationController
   def update
     if @item.user_id == current_user.id
       if @item.update(update_params)
-        binding.pry
         redirect_to root_path
       else
         render_edit
       end
 
     end
-      binding.pry
       redirect_to root_path
   end
 
